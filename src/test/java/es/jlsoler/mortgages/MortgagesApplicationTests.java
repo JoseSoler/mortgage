7ab94b94c0f1e436e11a30a942c884223ded0752
Jose Soler <josocas@gmail.com>
2024-10-24 11:07:19 +0200

    Initial commit from spring initlzr

---
+++ src/test/java/es/jlsoler/mortgages/MortgagesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jlsoler.mortgages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
