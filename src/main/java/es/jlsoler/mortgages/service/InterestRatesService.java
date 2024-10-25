130ca30badb5fa08f14ea6d4c785eb71c0ce1fb1
Jose Soler <josocas@gmail.com>
2024-10-25 10:45:51 +0200

    First milestone - walking skeleton

---
+++ src/main/java/es/jlsoler/mortgages/service/InterestRatesService.java
@@ -0,0 +1,18 @@
+package es.jlsoler.mortgages.service;
+
+import es.jlsoler.mortgages.api.InterestRatesApiDelegate;
+import es.jlsoler.mortgages.model.MortgageRate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InterestRatesService implements InterestRatesApiDelegate {
+
+    public ResponseEntity<List<MortgageRate>> listMortgageRates() {
+        return ResponseEntity.of(Optional.of(List.of(MortgageRate.builder().build())));
+    }
+
+}
